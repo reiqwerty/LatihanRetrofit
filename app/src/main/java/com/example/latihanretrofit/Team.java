c47505b99ca7d582faf4d82dec016057c282035a
reiqwerty <reiqwerty69@gmail.com>
2024-05-15 12:39:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanretrofit/Team.java
@@ -0,0 +1,32 @@
+package com.example.latihanretrofit;
+
+public class Team {
+    private String idTeam;
+    private String strTeam;
+    private String strTeamBadge;
+
+    public String getIdTeam() {
+        return idTeam;
+    }
+
+    public void setIdTeam(String idTeam) {
+        this.idTeam = idTeam;
+    }
+
+    public String getStrTeam() {
+        return strTeam;
+    }
+
+    public void setStrTeam(String strTeam) {
+        this.strTeam = strTeam;
+    }
+
+    public String getStrTeamBadge() {
+        return strTeamBadge;
+    }
+
+    public void setStrTeamBadge(String strTeamBadge) {
+        this.strTeamBadge = strTeamBadge;
+    }
+
+}
---
+++ app/src/main/java/com/example/latihanretrofit/TeamResponse.java
@@ -0,0 +1,11 @@
+package com.example.latihanretrofit;
+
+import java.util.List;
+
+public class TeamResponse {
+    private List<Team> teams;
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+}
